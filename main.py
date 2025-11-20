f5db175ba0466bf398d2d4deab850779e3d447e3
mgarciacelis <gcmiguelito2@gmail.com>
2025-11-20 02:41:40 +0000

    Added Area of a Circle

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+#1 Area of a Circle
+radius = float(input("Radius: "))
+from math import pi 
+area = round(pi * radius**2, 4)
+def circ_area(r):
+    return round(pi * r**2, 4)
+print("Area: " + str(area))
+area = circ_area(radius)
 
+#2 Expression Solver
