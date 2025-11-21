ad314b9a0a5d7450ef38f27649c98821c5890bbf
mgarciacelis <gcmiguelito2@gmail.com>
2025-11-21 03:41:21 +0000

    Added practices part 2 and 3

--- main.py
+++ main.py
@@ -8,3 +8,36 @@ print("Area: " + str(area))
 area = circ_area(radius)
 
 #2 Expression Solver
+a=int(input("what is the value for a?"))
+b=int(input("what is the value for b?"))
+print("Let's solve for (a-b)+(a*b):")
+answer=(a-b)+(a*b)
+print("Answer:"+str(answer))
+
+#3 ASCII Art
+print("ASCII art display here!")
+which_art=input("Do you want to see art#1 or art#2:")
+if which_art == "1":
+    print('''    
+`;-.          ___,
+  `.`\_...._/`.-"`
+    \        /      ,
+    /()   () \    .' `-._
+   |)  .    ()\  /   _.'
+   \  -'-     ,; '. <
+    ;.__     ,;|   > \
+   / ,    / ,  |.-'.-'
+  (_/    (_/ ,;|.<`
+    \    ,     ;-`
+     >   \    /
+    (_,-'`> .'
+   (_,   (_,    
+          ''')
+else:
+    print(''' 
+    _______
+   /      /,
+  /      //
+ /______//
+(______(/
+          ''')
